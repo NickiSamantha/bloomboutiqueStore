d5c3cc08064794ce06ded35f1c160c8fdce18771
NickiSamantha <nickisabels73@gmail.com>
2024-06-06 16:17:09 +0200

    products

--- script/code.js
+++ script/code.js
@@ -1,1 +1,75 @@
-//used for the home page 
+//create products and store it on the local storage
+let products = JSON.parse(localStorage.getItem("products")) ||
+  // ? JSON.parse(localStorage.getItem("products"))
+  
+    [
+        {
+          id: 1,
+          productName: "Kids Set",
+          category: "Sublimation",
+          description:
+            "Polymer cup , bowl , plate and cutlery set. Perfect for kids learning how to eat on their own",
+          amount: 350.0,
+          img_url: "https://nickisamantha.github.io/allImages/images/logo.png",
+        },
+        {
+          id: 2,
+          productName: "Mug",
+          category: "Sublimation",
+          description: "Ceremic glass mugs. Standard mug size that includes a custom print of your choice. ",
+          amount: 150.0,
+          img_url:
+            "https://nickisamantha.github.io/allImages/images/nickical.png",
+        },
+        {
+          id: 3,
+          productName: "Kids Tshirt",
+          category: "Vinyl Print",
+          description:
+            "Choose your favorite design, color and placement to have a custom designed T-shirt for kids. We have sizes available for all kids aged 12 months - 13 years old. All Tshirts are 100% Cotton. Tshirt color ranges are Red, Black, White , Grey, Blue and Yellow.",
+          amount: 250.0,
+          img_url: "https://nickisamantha.github.io/allImages/images/rin.png",
+        },
+        {
+          id: 4,
+          productName: "Adult Tshirt",
+          category: "Vinyl Print",
+          description:
+            "Choose your favorite design, color and placement to have a custom designed T-shirt for Adults. We have sizes available from SS - XXXL. All Tshirts are 100% Cotton. Tshirt color ranges are Red, Black, White , Grey, Blue and Yellow.",
+          amount: 350.0,
+          img_url:
+            "https://nickisamantha.github.io/allImages/images/nickikey.png",
+        },
+        {
+          id: 5,
+          productName: "Keyrings",
+          category: "Vinyl Print",
+          description: "",
+          amount: 350.0,
+          img_url:
+            "https://nickisamantha.github.io/allImages/images/nickiflex.png",
+        },
+      ]
+      localStorage.setItem("product", JSON.stringify(products))
+
+      function recentProducts() {
+        let arrSize = products.length
+        let latestProducts = products.slice(0, arrSize >> 1)
+        let wrapper = document.querySelector('[recentProducts]');
+        
+        latestProducts.forEach(product => {
+          wrapper.innerHTML += `
+        <div class="card"> 
+        <img src="${product.img_url}"
+        class="card-img-top" alt="${product.productName}" 
+        loading='lazy'>
+        <div class="card-body">
+        <h5 class="card-title">${product.productName}</h5>
+        <p class="card-text">${product.description}</p>
+  </div>
+</div>
+        `;
+        });
+      }
+      recentProducts()
+    
